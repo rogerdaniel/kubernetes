ed1df2eddeec1813721265bde5e31f56f8274706
Daniel Diaz <rogerdanieldiazz@gmail.com>
2021-11-12 15:19:10 -0300

    despliegue de infra con Terraform

---
+++ main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+variable "ssh_key_path" {}
+variable "vpc_id" {}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file(var.ssh_key_path)
+}
+
+resource "aws_security_group" "allowssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allowssh"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [
+    aws_security_group.allowssh.id
+  ]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "ip_instance" {
+  value = aws_instance.web.public_ip
+}
+
+output "ssh" {
+  value = "ssh -l ubuntu ${aws_instance.web.public_ip}"
+}
